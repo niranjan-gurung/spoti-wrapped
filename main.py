2533ff4269fd9ffa3c7f053464d31ad588293bb3
niranjan-gurung <gurung5130@gmail.com>
2025-03-10 19:16:36 +0000

    multiple json file upload -> redirect to new page, currently only prints out the filenames of selected files.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-  return "Hello Flask!"
+  return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def file_upload():
+  f = request.files.getlist('file')   # reads multiple file uploads - returns list
+  l = []
+  for item in f:
+    l.append(item.filename)
+  
+  return redirect(url_for('stats', fileList=l))
+
+@app.route('/stats/<fileList>')
+def stats(fileList):
+  return render_template('test.html', files=fileList)
