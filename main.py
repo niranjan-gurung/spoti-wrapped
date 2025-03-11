661b562a616dcbeef680c587ce14036a15c2ed17
niranjan-gurung <gurung5130@gmail.com>
2025-03-11 17:44:49 +0000

    parse selected file object to json, and render content on screen. Matplotlib integration, simple graph plotted and rendered on page.

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+from matplotlib.figure import Figure
+from io import BytesIO
+import base64
+import json
+
+# from contextlib import ExitStack
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+  if request.method == 'POST':
+  #   """TODO: open multiple files
+
+  #     files = request.files.getlist('file')
+  #     fnames = []
+  #     for name in files:
+  #       name.save(name.filename)
+  #       fnames.append(name.filename)
+  #     with ExitStack() as stack:
+  #       files = [
+  #         stack.enter_context(open(fname)) for fname in fnames
+  #       ]
+  #       print(files)
+  #       return render_template('test.html', data=files[0].buffer.read())
+  #   """
+
+    file = request.files['file']
+
+    if file:
+      file.save(file.filename)
+      with open(file.filename, encoding='utf8') as f:
+        d = json.load(f)    # serialize file object -> json
+
+        """
+        plot data points on graph, will be loaded as img.
+        plot demo line for now.
+        """
+        fig = Figure()
+        ax = fig.subplots()
+        ax.plot([1, 2])
+        
+        buf = BytesIO() # Save it to a temporary buffer.
+        fig.savefig(buf, format="png")
+        
+        d = base64.b64encode(buf.getbuffer()).decode("ascii") # Embed the result in the html output.
+        return render_template('test.html', data=d)
+
   return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def file_upload():
-  f = request.files.getlist('file')   # reads multiple file uploads - returns list
-  l = []
-  for item in f:
-    l.append(item.filename)
-  
-  return redirect(url_for('stats', fileList=l))
-
-@app.route('/stats/<fileList>')
-def stats(fileList):
-  return render_template('test.html', files=fileList)
